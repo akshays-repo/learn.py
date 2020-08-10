15d9f1d994e12e4aea132151d66de13ab261a79f
akshays-repo <akshayshan@gmail.com>
2020-08-10 19:29:44 +0530

    task2

--- task2.py
+++ task2.py
@@ -1,116 +1,114 @@
-print("-------T A S K - 2 ------ \n")
+#Take 3 int values from user  and print the greatest number among them.
+def questionOne():
+    a,b,c = input("enter 3 numbers . use spacebar ").split()
+    print(f'Three numbers are {a} {b} {c}')
+    if a > b and a > c:print(f'The greatest number is {a}')
+    elif b > a and b > c:print(f'The greatest number is {b}')
+    else:
+        print(f'The greates number is {c}')
 
 
-def stringMethods():
 
-        print("STRING METHODS \n")
-        string = "helLO WoRlD"
-        print(string.capitalize())
-        print(string.casefold())
-        print(string.center(20,"@"))
-        print(string.count("o"))
-        print(string.encode())
-        print(string.endswith("D"))
-        tabstring = "hello\tworld\thowareyou"
-        print(tabstring.expandtabs(20))
-        print(string.find("e"))
-        print(string.format())
-        print(string.isalnum())
-        print(string.isalpha())
-        print(string.isdecimal())
-        print(string.isdigit())
-        print(string.isidentifier())
-        print(string.islower())
-        print(string.isnumeric())
-        print(string.isprintable())
-        print(string.isspace())
-        print(string.istitle())
-        print(string.isupper())
-        print(string.join("-"))
-        print(string.lower())
+#Check whether a number is even or not.
+def questionTwo():
+    number = input("Enter the number")
+    if (int(number) % 2) == 0:print(f'Given number {number} is even number')
+    else:
+        print(f'Given number {number} is not a even number')
 
 
-def listMethods():
-    print("------LIST-----")
-    list = [1,2,232,"hello"]
-    list2 = [1,2,232,"hello"]
 
-    list.append("world")
-    print(list)
-
-    list2.clear()
-    print(list2)
-
-    list.copy()
-    print(list)
-
-    count = list.count(1)
-    print(count)  
-    
-    list3 = [2,3,4,5,4,3,2,5,6]
-    list.extend(list3)
-    print(list)
-    print()
-
-    list.insert(5,'problem')
-    print(list)
-
-    list.pop(5)
-    print(list)
-
-    list.reverse()
-    print(list)
-
-    list3.sort()
-    print(list3)
-
-def setMethods():
-
-    print('---SET---')
-
-    set = {123,'hello',23,43,3,'world'}
-    set.add('cat')
-    print(set)
-
-    set.copy()
-    print(set)
-
-    set.clear()
-    print(set)
-
-    set = {123,'hello',23,43,3,'world'}
-    set2 = {32,54,3,23,134 ,'hello','world'}
-
-    set3 = set.difference(set2)
-    print(set3)
-
-    set.difference_update(set2)
-    print(set)
-
-    set.discard(43)
-    print(set)
-
-    set3 = set.intersection(set2)
-    print(set3)
+# Check whether a number is positive, negative or zero.  
+def questionThree():
+    number = input("Enter the number")
+    if int(number) > 0:print(f'Given number {number} is even postive')
+    if int(number) < 0:print(f'Given number {number} is even negative')
+    else:
+        print(f'Given number {number} is even zero')
 
 
-def dictMethods():
-    print("----DICT---")
 
+#Take values of length and breadth of a rectangle from user and check if it is square or not.
+def questionFour():
+    length = input("Enter the length")
+    breadth = input("enter the breadth")
+    if int(length) == int(breadth):print("it is a SQUARE")
+    else:
+        print("Not a square")
+
+
+
+#A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years. Ask user for their salary and year of service and print the net bonus amount.
+def questionFive():
+    name = input("please enter employee name")
+    yearOfService = input("please enter year of service")
+    salary = input(f'please enter salary of {name}')
+    if (int(yearOfService) > 5):
+        new_salary = (21*int(salary))/20
+        print(f'You got a Hike{name} new salary is {new_salary}')
+    else:
+        print(f'sorry {name} you are not eligible for hike')
+
+
+
+#A school has following rules for grading system:
+def questionSix():
+    mark = input("Enter the Mark")
+
+    if(int(mark) > 80):print("The grade is A")
+    elif(int(mark)>60 and int(mark)<=80):print("The grade is B")
+    elif(int(mark)>50 and int(mark)<=60):print("The grade is C") 
+    elif(int(mark)>45 and int(mark)<=50):print("The grade is D") 
+    elif(int(mark)>25 and int(mark)<=45):print("The grade is E") 
+    else:
+        print("The grade is F")
+
+
+
+#A student will not be allowed to sit in exam if his/her attendence is less than 75%. Take following input from user:
+def questionSeven():
+    numberOfclasses = input("Enter the number of classes")
+    numberOfclassAttend = input("Enter the number of classes attended")
+    attendPercentage = (int(numberOfclassAttend)/int(numberOfclasses))*100
+    if (int(attendPercentage) < 75):
+        print(f'percentage of class attended is  {attendPercentage} and student is not allowed to sit in exam ')
+    else:
+        print(f'percentage of class attended is  {attendPercentage} and student is  allowed to sit in exam ')
+
+
+
+#Modify the above question to allow student to sit if he/she has medical cause. Ask user if he/she has medical cause or not ( 'Y' or 'N' ) and print accordingly.
+def questionEight():
+    numberOfclasses = input("Enter the number of classes")
+    numberOfclassAttend = input("Enter the number of classes attended")
+    attendPercentage = (int(numberOfclassAttend)/int(numberOfclasses))*100
+    if (int(attendPercentage) < 75):
+        n = input("Did you submit medical cetificate (Y/N)")
+        if( n == 'Y'):
+            print(f'percentage of class attended is  {attendPercentage} and student is  allowed to sit in exam ')
+        else:
+            print(f'percentage of class attended is  {attendPercentage} and student is not allowed to sit in exam ')
+            
+    else:print(f'percentage of class attended is  {attendPercentage} and student is  allowed to sit in exam ')
+
+
+
+print("----T A S K 2----\n")
+questionNumber = input("Enter the Question number :\n")
 
 while True:
-    choice = input(" \n\n 1 STRING OPERATIONS \n 2 LIST OPERATIONS \n 3 SET OPERATIONS \n 4 DICT OPERATIONS")
+    if (int(questionNumber) == 1):questionOne()
 
-    if int(choice) == 1:
-        stringMethods()
+    if (int(questionNumber) == 2):questionTwo()
 
-    if int(choice) == 2:
-        listMethods()
+    if (int(questionNumber) == 3):questionThree()
 
-    if int(choice) == 3:
-        setMethods()
+    if (int(questionNumber) == 4):questionFour()
 
-    if int(choice) == 4:
-        dictMethods()
+    if (int(questionNumber) == 5):questionFive()
 
+    if (int(questionNumber) == 6):questionSix()
 
+    if (int(questionNumber) == 7):questionSeven()
+
+    if (int(questionNumber) == 8):questionEight()
