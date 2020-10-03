40ce2272da11a2ac0efa48bb289410176c3b1202
akshays-repo <akshayshan28@gmail.com>
2020-10-03 09:27:26 +0530

    renamed

--- files.py/Neural Network/nn4.py
+++ files.py/Neural Network/nn4.py
@@ -1,48 +1,40 @@
+from keras import utils as np_utils
+from keras.layers import Dense
+from keras.models import Sequential
 import numpy as np
-import mnist
+from keras.datasets import mnist
 
-train_images = mnist.train.images()
-train_labels = mnist.train.labels()
-test_images = mnist.test.images()
-test_labels = mnist.test.labels()
-
-#normlise
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 train_images = (train_images / 255) - 0.5
 test_images = (test_images / 255) - 0.5
 
 
-#flatten
-train_images =train_images.reshape((-1 ,784))
-test_images =test_images.reshape((-1 ,784))
-
+# flatten
+train_images = train_images.reshape((-1, 784))
+test_images = test_images.reshape((-1, 784))
 print(train_images.shape)
 print(test_images.shape)
 
-form keras.models import Sequential
-from keras.layers import Dense
-
-#Build model
 
 model = Sequential([
-    Dense(64, activation='relu', input_shape=(784)),
+    Dense(64, activation='relu', input_shape=(784,)),
     Dense(64, activation='relu'),
     Dense(10, activation='softmax'),
-
 ])
-
-#compile
 
 model.compile(
     optimizer='adam',
     loss='categorical_crossentropy',
     metrics=['accuracy'])
 
-#Train
-model.fit(train_images,
-    to_categorical(train_label), batch_size=32, epochs=5)
-
+model.fit(
+    train_images,
+    np_utils.to_categorical(train_labels),
+    epochs=5,
+    batch_size=32,
+)
 model.evaluate(
     test_images,
-    to_categorical(test_labels)
+    np_utils.to_categorical(test_labels)
 )
